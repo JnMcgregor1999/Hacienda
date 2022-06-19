e51d7b49e3acfd6ac456a5fc08cd1e1973462c4e
Johan McGregor <47840123+JnMcgregor1999@users.noreply.github.com>
2022-06-19 16:50:40 -0600

    login and register
    only components

--- haciendaUTN/src/app/app-routing.module.ts
+++ haciendaUTN/src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    loadChildren: () => import('./auth/login/login.module').then((m) => m.LoginModule),
+    data: { title: "Login" }
+  },
+  {
+    path: "**",
+    redirectTo: ""
+  }
+
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  declarations: [],
+  imports: [RouterModule.forRoot(routes, { useHash: true, relativeLinkResolution: 'legacy' })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
